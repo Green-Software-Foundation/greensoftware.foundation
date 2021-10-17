61c2934523f1347a7f859ff0f703ea073252b51c
Osama Jandali <osamajandaly@gmail.com>
2021-10-18 01:24:43 +0300

    Add legal page

---
+++ src/pages/{datoCmsLegalPage.slug}.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { graphql, Link } from "gatsby";
+import { StructuredText } from "react-datocms";
+
+// Components
+import Layout from "../components/layout";
+import PageTitle from "../components/page-title";
+
+// Styles
+// import "../../styles/pages/working-groups.scss";
+
+const SingleLegalPageTemplate = ({ data: { datoCmsLegalPage: data } }) => {
+  return (
+    <Layout pageName="single-working-group">
+      <PageTitle>{data.title}</PageTitle>
+      <StructuredText data={data.content} />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query SingleLegalPageTemplateQuery($id: String) {
+    datoCmsLegalPage(id: { eq: $id }) {
+      id
+      slug
+      title
+      content {
+        value
+      }
+    }
+  }
+`;
+export default SingleLegalPageTemplate;
