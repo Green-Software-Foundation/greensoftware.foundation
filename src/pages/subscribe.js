f1a7a481bfc53af9fb7ba4fe9085eb21d2ab0143
Osama Jandali <osamajandaly@gmail.com>
2023-04-05 06:11:05 +0200

    add external links to WG and projects

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -7,6 +7,9 @@ import Layout from "../components/layout";
 import PageTitle from "../components/page-title";
 import Button from "../components/button";
 
+// Icons
+import ExternalLink from "../assets/icons/external-link.inline.svg"
+
 // Styles
 import "../styles/pages/onboarding.scss";
 function encode(data) {
@@ -19,9 +22,10 @@ const SubscribePage = ({ data: { allDatoCmsProjectV2: { nodes } } }) => {
   const [isSubmitted, setIsSubmitted] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const projects = nodes.map(node => node.name).sort(
-    (a, b) => a.localeCompare(b)
+  const projects = nodes.map(node => { return ({ name: node.name, url: node.url }) }).sort(
+    (a, b) => a.name.localeCompare(b.name)
   );
+  console.log(projects)
   const onSubmit = (data) => {
     fetch("/", {
       method: "POST",
@@ -90,6 +94,12 @@ const SubscribePage = ({ data: { allDatoCmsProjectV2: { nodes } } }) => {
                   {...register("Working Groups", { required: true })}
                 />
                 <label htmlFor="Checkbox_1">Standards</label>
+                <a href="https://standards.greensoftware.foundation/" target="_blank"
+                  rel="noopener noreferrer"
+                  className="external-link"
+                >
+                  <ExternalLink />
+                </a>
               </div>
               <div className="flex-align-center">
                 <input
@@ -100,6 +110,12 @@ const SubscribePage = ({ data: { allDatoCmsProjectV2: { nodes } } }) => {
                   {...register("Working Groups", { required: true })}
                 />
                 <label for="Checkbox_2">Policy</label>
+                <a href="https://policy.greensoftware.foundation/" target="_blank"
+                  rel="noopener noreferrer"
+                  className="external-link"
+                >
+                  <ExternalLink />
+                </a>
               </div>
               <div className="flex-align-center">
                 <input
@@ -110,6 +126,12 @@ const SubscribePage = ({ data: { allDatoCmsProjectV2: { nodes } } }) => {
                   {...register("Working Groups", { required: true })}
                 />
                 <label htmlFor="Checkbox_3">Opensource</label>
+                <a href="https://opensource.greensoftware.foundation/" target="_blank"
+                  rel="noopener noreferrer"
+                  className="external-link"
+                >
+                  <ExternalLink />
+                </a>
               </div>
               <div className="flex-align-center">
                 <input
@@ -120,20 +142,32 @@ const SubscribePage = ({ data: { allDatoCmsProjectV2: { nodes } } }) => {
                   {...register("Working Groups", { required: true })}
                 />
                 <label for="Checkbox_4">Community</label>
+                <a href="https://community.greensoftware.foundation/" target="_blank"
+                  rel="noopener noreferrer"
+                  className="external-link"
+                >
+                  <ExternalLink />
+                </a>
               </div>
             </div>
             <div className="checkboxes-wrapper">
               <p>Projects <span className="asterisk">*</span></p>
-              {projects.map((project, index) => (
+              {projects.map(({ name, url }, index) => (
                 <div className="flex-align-center" key={index}>
                   <input
                     type="checkbox"
-                    id={`Checkbox_${project}`}
+                    id={`Checkbox_${name}`}
                     name="Projects"
-                    value={project}
+                    value={name}
                     {...register("Projects", { required: true })}
                   />
-                  <label for={`Checkbox_${project}`}>{project}</label>
+                  <label for={`Checkbox_${name}`}>{name}</label>
+                  <a href={url} target="_blank"
+                    rel="noopener noreferrer"
+                    className="external-link"
+                  >
+                    <ExternalLink />
+                  </a>
                 </div>
               ))}
             </div>
@@ -166,6 +200,7 @@ export const query = graphql`
     allDatoCmsProjectV2(sort: {fields: name, order: ASC}) {
       nodes {
         name
+        url
       }
   }
   }
