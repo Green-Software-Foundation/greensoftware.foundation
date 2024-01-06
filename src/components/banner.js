110d496bd87ad07539564594f3e3e9ce039a7b1c
Osama Jandali <osamajandaly@gmail.com>
2024-01-06 23:42:17 +0200

    Update Carbon Hack dates in banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
             </div>
             <div className="center">
                 <img src={Logo} alt="Carbon Hack logo" />
-                <p>Join Carbon Hack 24 on 24 February - 14 March
+                <p>Join Carbon Hack 24 on 18 March - 8 April
                     {/* <strong>Survey ends December 31<sup>st</sup></strong> */}
                 </p>
                 <a href="https://grnsft.org/hack/homepage" target="_blank"
