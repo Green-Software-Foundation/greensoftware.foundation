e10be3cbabab163624fc5cf21def65f124ba2dbd
Russell Trow <russell@greensoftware.foundation>
2025-02-10 15:29:34 +0000

    Update banner.js
    Signed-off-by: Russell Trow <russell@greensoftware.foundation>

--- src/components/banner.js
+++ src/components/banner.js
@@ -14,10 +14,8 @@ const Banner = () => {
             </div>
             <div className="center">
                 <img src={Logo} alt="Banner logo" />
-                <p>SSIA joins the Green Software Foundation</p>
-                <a href="https://greensoftware.foundation/articles/sustainable-and-scalable-infrastructure-alliance-joins-the-green-software-foundat" target="_blank"
-                    rel="noopener noreferrer"
-                >Read more</a>
+                <p>We are hiring! Come and join the GSF Team!</p>
+                <a Tzhref="https://wiki.greensoftware.foundation/job-board" target="_blank" rel="noopener noreferrer">Read more</a>
             </div>
             <div className="right">
 
