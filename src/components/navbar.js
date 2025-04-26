8b9b4b975d65345c87bef5d2b953a6bc2ce2a7fa
Sean Mcilroy <smcilroy@linuxfoundation.org>
2025-04-26 10:34:26 +0100

    Update navbar.js
    Signed-off-by: Sean Mcilroy <smcilroy@linuxfoundation.org>

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -94,7 +94,7 @@ const menuData = [
       {
         title: "Software Carbon Intensity (SCI) Specification",
         href: "https://sci.greensoftware.foundation/",
-      }
+      },
       {
         title: "Policy Radar",
         href: "https://policy-radar.greensoftware.foundation/",
